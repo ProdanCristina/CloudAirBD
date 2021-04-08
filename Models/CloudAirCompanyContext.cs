aa146b42da3d5947521f90f9e83f14e4b84ef7fc
ProdanCristina <prodan.andreea.z2t@student.ucv.ro>
2021-04-08 21:05:52 +0300

    Baza de date

---
+++ Models/CloudAirCompanyContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudAirBD.Models
+{
+   
+        public class CloudAirCompanyContext : DbContext
+        {
+            public CloudAirCompanyContext(DbContextOptions<CloudAirCompanyContext> options)
+                : base(options)
+            {
+
+            }
+
+            public DbSet<Booking> Bookings { get; set; }
+
+            public DbSet<CloudAirCompany> CloudAirCompanies { get; set; }
+
+            public DbSet<User> Users { get; set; }
+
+            public DbSet<Ticket> Tickets { get; set; }
+        }
+    }
+
